38a544eb012f7cf385bc75a6083bda4bf64b264b
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2024-11-25 15:00:59 +0200

    code cleanup

--- src/bot.ts
+++ src/bot.ts
@@ -127,7 +127,7 @@ export const robot = (app: Probot) => {
           // without checking any previous owners
           const apiUrl = getApiUrl();
 
-          //await checkIfTickerAlreadyExists(token);
+          await checkIfTickerAlreadyExists(token);
 
           await checkExtraTokensFields(files, token);
 
@@ -145,9 +145,6 @@ export const robot = (app: Probot) => {
             const { data: infoFromPullRequest } = await axios.get(infoJsonFile.raw_url);
 
             if (infoFromPullRequest && typeof infoFromPullRequest === 'object' && infoFromPullRequest['lockedAccounts']) {
-              console.log(infoFromPullRequest['lockedAccounts']);
-              console.log(infoFromPullRequest.lockedAccounts);
-              console.log(typeof infoFromPullRequest.lockedAccounts);
               for (const lockedAccount in infoFromPullRequest.lockedAccounts) {
                 if (lockedAccount.length !== 62) {
                   await fail(`Invalid locked accounts: ${lockedAccount}`);
@@ -156,7 +153,6 @@ export const robot = (app: Probot) => {
             }
 
             if (infoFromPullRequest && typeof infoFromPullRequest === 'object' && infoFromPullRequest['extraTokens']) {
-              console.log(infoFromPullRequest.extraTokens);
               for (const extra of infoFromPullRequest.extraTokens) {
                 if (!validateTokenIdentifier(extra)) {
                   await fail(`Invalid extra token: ${extra}`);
@@ -263,33 +259,33 @@ export const robot = (app: Probot) => {
           return [...new Set(tokens)];
         }
 
-        // async function checkIfTickerAlreadyExists(tokenName: string) {
-        //   const tokensDirPath = (network === "mainnet") ? "/tokens" : `/${network}/tokens`;
-        //   const response = await context.octokit.repos.getContent({
-        //     owner: context.repo().owner,
-        //     repo: context.repo().repo,
-        //     path: tokensDirPath,
-        //   });
-        //
-        //   const tokenTicker = tokenName.split("-")[0];
-        //   if (!tokenTicker) {
-        //     return;
-        //   }
-        //   const tokensDirectories = (response.data && (response.data as any[]).length) ? response.data as any[] : [];
-        //   const subdirectoriesWithSameTicker = tokensDirectories.filter(
-        //     (content) => content?.type === "dir" && content?.name?.startsWith(`${tokenTicker}-`),
-        //   );
-        //
-        //   if (subdirectoriesWithSameTicker.length) {
-        //     const existingToken = subdirectoriesWithSameTicker[0].name;
-        //     if (existingToken === tokenName) {
-        //       // it's just an update to the same token
-        //       return;
-        //     }
-        //
-        //     await fail(`Token with ticker ${tokenTicker} already exists! Existing token id=${existingToken}. New token id=${tokenName}`);
-        //   }
-        // }
+        async function checkIfTickerAlreadyExists(tokenName: string) {
+          const tokensDirPath = (network === "mainnet") ? "/tokens" : `/${network}/tokens`;
+          const response = await context.octokit.repos.getContent({
+            owner: context.repo().owner,
+            repo: context.repo().repo,
+            path: tokensDirPath,
+          });
+
+          const tokenTicker = tokenName.split("-")[0];
+          if (!tokenTicker) {
+            return;
+          }
+          const tokensDirectories = (response.data && (response.data as any[]).length) ? response.data as any[] : [];
+          const subdirectoriesWithSameTicker = tokensDirectories.filter(
+            (content) => content?.type === "dir" && content?.name?.startsWith(`${tokenTicker}-`),
+          );
+
+          if (subdirectoriesWithSameTicker.length) {
+            const existingToken = subdirectoriesWithSameTicker[0].name;
+            if (existingToken === tokenName) {
+              // it's just an update to the same token
+              return;
+            }
+
+            await fail(`Token with ticker ${tokenTicker} already exists! Existing token id=${existingToken}. New token id=${tokenName}`);
+          }
+        }
 
         async function fail(reason: string) {
           await createComment(reason);
